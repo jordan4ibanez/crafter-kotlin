7c7ed5aa6525933f73658f9e64d083f087d0a5a6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-20 21:26:31 -0400

    Enable :run default

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+var i = 1
+
+
+
+
+fun main(args: Array<String>) {
+  i++
+  print(i)
 }
