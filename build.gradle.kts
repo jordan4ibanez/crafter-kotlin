dabe076c8c8543fe7a77c3493de125029b63c764
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-21 01:15:29 -0400

    Get a base project going again

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.internal.os.OperatingSystem
+val lwjglVersion = "3.3.3"
+val jomlVersion = "1.10.5"
+
 plugins {
     kotlin("jvm") version "1.9.0"
     application
@@ -5,6 +9,21 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+val lwjglNatives = Pair(
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        arrayOf("Linux", "FreeBSD", "SunOS", "Unit").any { name.startsWith(it) } ->
+            "natives-linux"
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }                ->
+            "natives-macos"
+        arrayOf("Windows").any { name.startsWith(it) }                           ->
+            "natives-windows"
+        else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+    }
+}
 
 repositories {
     mavenCentral()
