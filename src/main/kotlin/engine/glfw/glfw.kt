8b90c8ab5eb26541435ed26076ed8bb085d2d3e4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-21 06:05:58 -0400

    On a roll here

--- src/main/kotlin/engine/glfw/glfw.kt
+++ src/main/kotlin/engine/glfw/glfw.kt
@@ -12,6 +12,7 @@ object glfw {
 
   private val monitorSize = Vector2i()
   private var windowPointer: Long = NULL
+  private const val WINDOW_TITLE = "Crafter"
 
   fun initialize() {
 
@@ -44,10 +45,21 @@ object glfw {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE)
 
-    run {
-      val (sizeX, sizeY) = getMonitorSize().destructure()
-      windowPointer = glfwCreateWindow(sizeX / 2, sizeY / 2, "Whatever the game is called", NULL, NULL)
-    }
+    constructWindow()
+
+    glfwMakeContextCurrent(windowPointer)
+
+    glfwSwapInterval(1)
+
+    glfwShowWindow(windowPointer)
+
+  }
+
+  private fun constructWindow() {
+    val (monitorSizeX, monitorSizeY) = getMonitorSize().destructure()
+    val (windowSizeX, windowSizeY) = arrayOf(monitorSizeX / 2, monitorSizeY / 2)
+    val (windowPosX, windowPosY) = arrayOf((monitorSizeX - windowSizeX) / 2, (monitorSizeY - windowSizeY) / 2)
+    windowPointer = glfwCreateWindow(windowSizeX, windowSizeY, WINDOW_TITLE, NULL, NULL)
 
     // Now if this gets called, we have a serious problem.
     if (windowPointer == NULL) {
@@ -55,12 +67,11 @@ object glfw {
     }
 
     glfwSetFramebufferSizeCallback(windowPointer) { _, width, height ->
-      println("Window was resized to: $width, $height")
+      println("Framebuffer was resized to: $width, $height")
       glViewport(0, 0, width, height)
     }
 
-
-
+    glfwSetWindowPos(windowPointer, windowPosX, windowPosY)
   }
 
   fun getMonitorSize(): Vector2ic {
