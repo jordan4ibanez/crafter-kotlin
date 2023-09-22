43e15a0bc5603f03e4d6be8f44c5878890ebbd54
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-22 02:55:19 -0400

    Now that's real nice

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,35 @@
 import engine.gl
 import engine.glfw
+import engine.window
 
 fun main(args: Array<String>) {
   println(args)
 
+  initialization()
+  gameLoop()
+  destruction()
+
+}
+
+fun initialization() {
   glfw.initialize()
   gl.initialize()
+}
 
+fun mainLogic() {
 
+}
 
-
+fun destruction() {
   glfw.destroy()
+}
 
+
+tailrec fun gameLoop() {
+  mainLogic()
+
+  if (window.shouldClose()){
+    return
+  }
+  return gameLoop()
 }
