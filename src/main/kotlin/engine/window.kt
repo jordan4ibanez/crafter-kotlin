0011bbdb977dbf45c9731de96c515f870dbe41de
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-23 05:18:23 -0400

    Now automate time calculation

--- src/main/kotlin/engine/window.kt
+++ src/main/kotlin/engine/window.kt
@@ -46,9 +46,7 @@ object window {
   }
 
   fun update() {
-
-    // Needs to calculate delta here.
-
+    delta.calculate()
     glfwPollEvents()
     glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
   }
