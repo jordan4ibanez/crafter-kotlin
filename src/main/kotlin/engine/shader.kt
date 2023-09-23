46ecd2ac2ca685d1690f011d2024fe8a1436cd9d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-23 06:47:56 -0400

    Fill shader out a bit

--- src/main/kotlin/engine/shader.kt
+++ src/main/kotlin/engine/shader.kt
@@ -1,6 +1,7 @@
 package engine
 
-import org.lwjgl.opengl.GL20.glCreateProgram
+import org.lwjgl.opengl.GL20.*
+import java.io.File
 
 object shader {
 
@@ -8,29 +9,71 @@ object shader {
 
 private class Shader {
   val name: String
-  val id: Int
+  val programID: Int
   val uniforms = HashMap<String, Int>()
 
   constructor(name: String, vertexSourceCodeLocation: String, fragmentSourceCodeLocation: String) {
     this.name = name
-    id = createShader()
+    programID = createShader(vertexSourceCodeLocation, fragmentSourceCodeLocation)
   }
 
 }
 
-private fun createShader(): Int {
-  val id = glCreateProgram()
+private fun createShader(vertexSourceCodeLocation: String, fragmentSourceCodeLocation: String): Int {
+  val programID = glCreateProgram()
 
-  if (id == 0) {
+  if (programID == 0) {
     throw RuntimeException("Shader: Failed to create shader program.")
   }
 
-  //todo compilation goes here
+  val vertexID = compileSourceCode(programID, vertexSourceCodeLocation, GL_VERTEX_SHADER)
+  val fragmentID = compileSourceCode(programID, fragmentSourceCodeLocation, GL_FRAGMENT_SHADER)
 
+  link(programID, vertexID, fragmentID)
 
-  //todo linkage goes here
-
-  return id
+  return programID
 }
 
-private fun compileSourceCode()
+private fun compileSourceCode(programID: Int, sourceCodeLocation: String, shaderType: Int): Int {
+  val sourceCode = getFileString(sourceCodeLocation)
+
+  val shaderID = glCreateShader(shaderType)
+
+  if (shaderID == 0) {
+    val shaderTypeString = if (shaderType == GL_VERTEX_SHADER) "GL_VERTEX_SHADER" else "GL_FRAGMENT_SHADER"
+    throw RuntimeException("compileSourceCode: Failed to create shader $shaderTypeString at $sourceCodeLocation")
+  }
+
+  glShaderSource(shaderID, sourceCode)
+
+  glCompileShader(shaderID)
+
+  if (glGetShaderi(shaderID, GL_COMPILE_STATUS) == GL_FALSE) {
+    throw RuntimeException("compileSourceCode: Error compiling. ${glGetShaderInfoLog(shaderID)}")
+  }
+
+  glAttachShader(programID, shaderID)
+
+  return shaderID
+}
+
+private fun link(programID: Int, vertexID: Int, fragmentID: Int) {
+  glLinkProgram(programID)
+
+  if (glGetProgrami(programID, GL_LINK_STATUS) == GL_FALSE) {
+    throw RuntimeException("link: Failed to link program. ${glGetProgramInfoLog(programID)}")
+  }
+
+  glDetachShader(programID, vertexID)
+  glDetachShader(programID, fragmentID)
+
+  //todo: Test this
+//  glDeleteShader(vertexID)
+//  glDeleteShader(fragmentID)
+
+  glValidateProgram(programID)
+
+  if (glGetProgrami(programID, GL_VALIDATE_STATUS) == GL_FALSE) {
+    throw RuntimeException("link: Validation failed. ${glGetProgramInfoLog(programID)}")
+  }
+}
