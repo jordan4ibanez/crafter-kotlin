ba2bad28b59ca5cda840601aeee48f24198b166c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-24 05:55:53 -0400

    Now there is a basic camera

--- src/main/kotlin/engine/camera.kt
+++ src/main/kotlin/engine/camera.kt
@@ -3,7 +3,9 @@ package engine
 import org.joml.Math
 import org.joml.Math.toRadians
 import org.joml.Matrix4f
+import org.joml.Vector2fc
 import org.joml.Vector3f
+import org.joml.Vector3fc
 
 
 object camera {
@@ -28,6 +30,44 @@ object camera {
     shader.setUniform("cameraMatrix", cameraMatrix)
   }
 
+  fun setObjectMatrix(position: Vector3fc, rotation: Vector3fc, scale: Vector3fc) {
+    objectMatrix
+      .identity()
+      .translate(
+        position.x() - this.position.x(),
+        position.y() - this.position.y(),
+        position.z() - this.position.z())
+      .rotateY(-rotation.y())
+      .rotateX(-rotation.x())
+      .rotateZ(-rotation.z())
+      .scale(scale)
+
+    shader.setUniform("objectMatrix", objectMatrix)
+  }
+
+  fun updateGUICameraMatrix() {
+    val (width, height) = window.getFrameBufferSize().destructure()
+
+    guiCameraMatrix
+      .identity()
+      .setOrtho2D(0f, width.toFloat(), height.toFloat(), 0f)
+
+    shader.setUniform("cameraMatrix", guiCameraMatrix)
+  }
 
 
+  fun setGUIObjectMatrix(posX: Float, posY: Float) {
+    setGUIObjectMatrix(posX, posY, 1f, 1f)
+  }
+
+  fun setGUIObjectMatrix(posX: Float, posY: Float, scaleX: Float, scaleY: Float) {
+    guiObjectMatrix
+      .identity()
+      .translate(posX, posY, 0f)
+      .scale(scaleX, scaleY, 1f)
+
+    shader.setUniform("objectMatrix", guiObjectMatrix)
+  }
+
+  // Todo: Put the rest of the functionality in here.
 }
