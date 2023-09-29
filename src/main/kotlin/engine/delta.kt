230379cb8c60f96878136cdd3ed75a0dd03b05bd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-29 04:36:37 -0400

    Automate FPS calculation

--- src/main/kotlin/engine/delta.kt
+++ src/main/kotlin/engine/delta.kt
@@ -7,15 +7,42 @@ internal object delta {
   internal var deltaTime: Float = 0f
   private var oldTime: Long = System.nanoTime()
 
+  internal var fps = 0
+  private var fpsAccumulator = 0
+  private var fpsTime = 0f
+  internal var fpsUpdate = false
+
   internal fun calculate() {
     // No other packages should be able to touch this besides engine.
     val currentTime = System.nanoTime()
     deltaTime = (currentTime - oldTime).toFloat() / 1_000_000_000f
     oldTime = currentTime
+
+    fpsCalculate()
+  }
+
+  private fun fpsCalculate() {
+    fpsUpdate = false
+    fpsTime += deltaTime
+    if (fpsTime >= 1f) {
+      fpsTime -= 1f
+      fps = fpsAccumulator
+      fpsUpdate = true
+      fpsAccumulator = 0
+    }
+    fpsAccumulator++;
   }
 }
 
 // Hook this up as a general function. An alias so you do not have to do: delta.getDelta()
 fun getDelta(): Float {
   return delta.deltaTime
+}
+
+fun fpsUpdated(): Boolean {
+  return delta.fpsUpdate
+}
+
+fun getFPS(): Int {
+  return delta.fps
 }
