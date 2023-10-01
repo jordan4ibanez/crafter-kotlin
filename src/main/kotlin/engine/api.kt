f9b0ad6ca7a6d268513608a4a00b7f92bab498ec
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-01 12:46:47 -0400

    Revert "We're gonna try this one more time"
    This reverts commit 598ce38a8d60d89262f705be91bc96dd5fa828e5.

--- src/main/kotlin/engine/api.kt
+++ src/main/kotlin/engine/api.kt
@@ -11,37 +11,37 @@ api works as a state machine.
 
 object api {
 
-////  note: val for now, var when main menu gets put it so it can be reloaded. Or maybe not. We'll see.
-//  private val javaScript = NashornScriptEngineFactory().getScriptEngine("--language=es6")!!
-//  private val bindings = javaScript.getBindings(ScriptContext.ENGINE_SCOPE)
-//  private val compiler = javaScript as Compilable
-//  private val invoker = javaScript as Invocable
-//
-//  // One day
-////  private val tsCompiler =
-//
-//  fun initialize() {
-//
-//    // create the api.
-//    runFile("./api/api.js")
-//
-//  }
-//
-//  private fun runFile(fileLocation: String) {
-//    runCode(getFileString(fileLocation))
-//  }
-//
-//  private fun runCode(rawCode: String) {
-//    try { javaScript.eval(rawCode) } catch (e: Exception) { throw RuntimeException("(Javascript API error):\n$e") }
-//  }
-//
-//  private fun invoke(functionName: String, vararg args: Any): Any {
-//    try { return invoker.invokeFunction(functionName, args) } catch (e: Exception) { throw RuntimeException("(Javascript API error):\n$e") }
-//  }
-//
-//  fun debugTest() {
-//    println("api debug successful.")
-//  }
+//  note: val for now, var when main menu gets put it so it can be reloaded. Or maybe not. We'll see.
+  private val javaScript = NashornScriptEngineFactory().getScriptEngine("--language=es6")!!
+  private val bindings = javaScript.getBindings(ScriptContext.ENGINE_SCOPE)
+  private val compiler = javaScript as Compilable
+  private val invoker = javaScript as Invocable
+
+  // One day
+//  private val tsCompiler =
+
+  fun initialize() {
+
+    // create the api.
+    runFile("./api/api.js")
+
+  }
+
+  private fun runFile(fileLocation: String) {
+    runCode(getFileString(fileLocation))
+  }
+
+  private fun runCode(rawCode: String) {
+    try { javaScript.eval(rawCode) } catch (e: Exception) { throw RuntimeException("(Javascript API error):\n$e") }
+  }
+
+  private fun invoke(functionName: String, vararg args: Any): Any {
+    try { return invoker.invokeFunction(functionName, args) } catch (e: Exception) { throw RuntimeException("(Javascript API error):\n$e") }
+  }
+
+  fun debugTest() {
+    println("api debug successful.")
+  }
 }
 
 fun test() {
