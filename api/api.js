666bbe77b715302bc2ccdf62693d09c160794588
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-05 06:04:27 -0400

    Completely overhaul the JS api

--- api/api.js
+++ api/api.js
@@ -30,228 +30,81 @@ Possible implementations: Typescript (one day)
 
 // Global java types assignment.
 //
-// const BlockDefinition = Java.type("engine.BlockDefinition")
-const DrawType = Java.type("engine.DrawType")
-//const BiomeDefinition = Java.type("engine.world.biome.BiomeDefinition")
-//const Player = Java.type("org.crafter.game.entity.player.Player")
-// Script actions.
-//const OnJoin = Java.type("engine.api.actions.on_join.OnJoin")
-//const OnTick = Java.type("engine.api.actions.on_tick.OnTick")
-//const OnTimer = Java.type("engine.api.actions.on_timer.OnTimer")
+
+const api = Java.type("engine.api").INSTANCE
+const block = Java.type("engine.block").INSTANCE
+const fileHelpers = Java.type("engine.File_helpersKt")
+
+/**
+ * A block DrawType enumerator.
+ */
+const DrawType = {
+  AIR: Java.type("engine.DrawType").AIR,
+  BLOCK: Java.type("engine.DrawType").BLOCK,
+  BLOCK_BOX: Java.type("engine.DrawType").BLOCK_BOX,
+  TORCH: Java.type("engine.DrawType").TORCH,
+  LIQUID_SOURCE: Java.type("engine.DrawType").LIQUID_SOURCE,
+  LIQUID_FLOR: Java.type("engine.DrawType").LIQUID_FLOW,
+  GLASS: Java.type("engine.DrawType").GLASS,
+  PLANT: Java.type("engine.DrawType").PLANT,
+  LEAVES: Java.type("engine.DrawType").LEAVES
+};
+
+// Lua equivalents
+//
+/**
+ * Execute a file.
+ * @param {string} fileDirectory 
+ */
+const dofile = function(fileDirectory) { api.runFile(fileDirectory) };
+
+/**
+ * Execute raw code.
+ * @param {string} rawCode 
+ */
+const loadstring = function(rawCode) { api.runCode(rawCode) };
+
+/** 
+ * Get string of a text file.
+ * @param {string} fileLocation
+ */
+const getFileString = fileHelpers.getFileString;
+
+/**
+ * JOML math library.
+ */
+const math = Java.type("org.joml.Math");
+
+// Kotlin equivalents.
+//
+const println = print;
 
 // JOML types
-//
+// Mutable.
 const Vector2f = Java.type("org.joml.Vector2f");
 const Vector3f = Java.type("org.joml.Vector3f");
 const Vector2i = Java.type("org.joml.Vector2i");
 const Vector3i = Java.type("org.joml.Vector3i");
-// Readonly
+// Readonly.
 const Vector2fc = Java.type("org.joml.Vector2fc");
 const Vector3fc = Java.type("org.joml.Vector3fc");
 const Vector2ic = Java.type("org.joml.Vector2ic");
 const Vector3ic = Java.type("org.joml.Vector3ic");
 
-// Very similar to minetest's api table, basically a clone of it in JS.
-const crafter = [];
+const crafter = {
 
-// Block Manipulator gets it's own table for minor performance and clarity.
-const blockManipulator = [];
-
-// Similar component to minetest's block manipulator data worker, but integrated into it's own table.
-const blockData = [];
-
-// Stores functions for the Block Definition Container.
-const blockDefinition = [];
-
-// Auto executing lambda localized variable scope discards
-(function(){
-
-  // Objects from the engine which will be referenced after this scope ends.
-  const api = Java.type("engine.api").INSTANCE
-  const block = Java.type("engine.block").INSTANCE
-  const fileHelpers = Java.type("engine.File_helpersKt")
-
-  // const ChunkStorage = Java.type("engine.world.chunk.ChunkStorage")
-  // const ActionStorage = Java.type("engine.api.ActionStorage")
-  // const PlayerStorage = Java.type("org.crafter.game.entity.player.PlayerStorage")
-  // const Chunk = Java.type("engine.world.chunk.Chunk")
-  // const BitManipulation = Java.type("engine.world.chunk.ChunkBitManipulation")
-
-  // Lua equivalents.
-  // Shove these into the crafter array, then globalize them later.
-  crafter.dofile = function(fileDirectory) { api.runFile(fileDirectory) }
-  crafter.loadstring = function(rawCode) { api.runCode(rawCode) }
-
-  crafter.getFileString = fileHelpers.getFileString
-  crafter.math = Java.type("org.joml.Math")
-
-  //fixme: id needs to be dispatched internally.
-  crafter.registerBlock = function(id, name, inventoryName, textures, drawtype) {
-     block.newBlock(id, name, inventoryName, textures, drawtype)
+  //!fixme: id needs to be dispatched internally.
+  /**
+   * Register a block into the game.
+   * @param {number} id 
+   * @param {string} name 
+   * @param {string} inventoryName 
+   * @param {Array<string>} textures 
+   * @param {DrawType} drawtype 
+   */
+  registerBlock: function(id, name, inventoryName, textures, drawtype) {
+    block.newBlock(id, name, inventoryName, textures, drawtype)
   }
+}
 
-
-  // // Javascript level Biome Definition registration function.
-  // // This is why I avoid singletons, cannot reduce this.
-  // crafter.registerBiome = function(newBiomeDefinition) {
-  //     BiomeDefinitionContainer.getMainInstance().registerBiome(newBiomeDefinition)
-  // }
-
-  // //FIXME!!! THIS NEEDS TO BE FLESHED OUT!
-  // //FIXME!!! INCLUDE ALL THE METHODS FROM THE BLOCK DEFINITIONS!
-  // blockDefinition.getID = function(input) {
-  //     return BlockDefinitionContainer.getMainInstance().getDefinition(input).getID()
-  // }
-
-  // // Will get the mod directory of the current mod.
-  // // Returns: String
-  // crafter.getCurrentModDirectory = API.getCurrentModDirectory
-
-  // // Gets if a chunk is loaded in a position in the world.
-  // // Parameters: [x,y,z or Vector3f]
-  // // Returns: boolean
-  // crafter.isChunkLoaded = ChunkStorage.isChunkLoaded
-
-  // // Gets an iterable collection of all players currently online.
-  // // Returns: Collection<Player>
-  // crafter.getConnectedPlayers = PlayerStorage.getConnectedPlayers
-
-  // //fixme ----- BEGIN SINGLE BLOCK API -----
-
-  // // Get a block's RAW data using a raw in world position. (Using this in bulk can be very expensive)
-  // // ONLY USE THIS IF YOU KNOW WHAT YOU ARE DOING!
-  // // Parameters: [x,y,z or Vector3f]
-  // // Returns: integer
-  // crafter.getBlockRAW = ChunkStorage.getBlockRAW
-
-  // // Get a block's internal name using a raw in world position. (Using this in bulk can be very expensive)
-  // // Parameters: [x,y,z or Vector3f]
-  // // Returns: String
-  // crafter.getBlockName = ChunkStorage.getBlockName
-
-  // // Get a block's ID using a raw in world position. (Using this in bulk can be very expensive)
-  // // Parameters: [x,y,z or Vector3f]
-  // // Returns: integer
-  // crafter.getBlockID = ChunkStorage.getBlockID
-
-  // // Get a block's light level using a raw in world position. (Using this in bulk can be very expensive)
-  // // Parameters: [x,y,z or Vector3f]
-  // // Returns: integer
-  // crafter.getBlockLightLevel = ChunkStorage.getBlockLightLevel
-
-  // // Get a block's state using a raw in world position. (Using this in bulk can be very expensive)
-  // // Parameters: [x,y,z or Vector3f]
-  // // Returns: integer
-  // crafter.getBlockState = ChunkStorage.getBlockState
-
-  // // Set a block's RAW data using a raw in world position. (Using this in bulk can be very expensive)
-  // // ONLY USE THIS IF YOU KNOW WHAT YOU ARE DOING!
-  // // Parameters: [x,y,z or Vector3f] [integer]
-  // crafter.setBlockRAW = ChunkStorage.setBlockRAW
-
-  // // Set a block's ID with the internal name of the block using a raw in world position. (Using this in bulk can be very expensive)
-  // // Parameters: [x,y,z or Vector3f] [String]
-  // crafter.setBlockName = ChunkStorage.setBlockName
-
-  // // Set a block's ID using a raw in world position. (Using this in bulk can be very expensive)
-  // // Parameters: [x,y,z or Vector3f] [integer]
-  // crafter.setBlockID = ChunkStorage.setBlockID
-
-  // // Set a block's light level using a raw in world position. (Using this in bulk can be very expensive)
-  // // Parameters: [x,y,z or Vector3f] [integer]
-  // crafter.setBlockLightLevel = ChunkStorage.setBlockLightLevel
-
-  // // Set a block's state using a raw in world position. (Using this in bulk can be very expensive)
-  // // Parameters: [x,y,z or Vector3f] [integer]
-  // crafter.setBlockState = ChunkStorage.setBlockState
-
-  // //fixme ----- BEGIN ACTIONS API -----
-
-  // // Run some logic when a player joins the game.
-  // // Parameters: [OnJoin]
-  // crafter.registerOnJoin = ActionStorage.registerOnJoin
-
-  // // Run some logic every game tick.
-  // // Parameters: [OnTick]
-  // crafter.registerOnTick = ActionStorage.registerOnTick
-
-  // // Run some logic at X second intervals. Or, execute it after X seconds if repeat is off.
-  // // Parameters:
-  // // [float] interval OR delay. (depends if repeat is on)
-  // // [boolean] Repeat. If true, this function will run every X seconds. If false, it will run once, then be deleted.
-  // // [OnTimer] OnTimer function.
-  // crafter.registerOnTimer = ActionStorage.registerOnTimer
-
-  // //fixme ---- BEGIN BLOCK MANIPULATOR API ----
-
-  // // Set the min and max positions of the Block Manipulator.
-  // // Parameters: [Vector3i] [Vector3i] OR  min: [int] [int] [int] max: [int] [int] [int]
-  // blockManipulator.setPositions = ChunkStorage.setBlockManipulatorPositions
-
-  // // Make the block manipulator read the map.
-  // blockManipulator.readData = ChunkStorage.blockManipulatorReadData
-
-  // // Get RAW data from the Block Manipulator.
-  // // Parameters: [Vector3i] OR [int] [int] [int]
-  // blockManipulator.getData = ChunkStorage.getBlockManipulatorData
-
-  // // Set RAW data into the Block Manipulator.
-  // // Parameters: [Vector3i] [int] OR [int] [int] [int] [int]
-  // blockManipulator.setData = ChunkStorage.setBlockManipulatorData
-
-  // // Write the Block Manipulator's data into the map.
-  // blockManipulator.writeData = ChunkStorage.writeManipulatorWriteData
-
-  // //fixme ----- BEGIN BLOCK DATA MANIPULATOR API -----
-
-  // // Get the Block ID from raw Block data.
-  // // Parameters: [int] Raw block data.
-  // // Returns: [int] Block ID.
-  // blockData.getID = BitManipulation.getBlockID
-
-  // print("COOL TEST = " + blockData.getID)
-
-  // // Get the Block light level from raw Block data.
-  // // Parameters: [int] Raw block data.
-  // // Returns: [int] Block light level.
-  // blockData.getLightLevel = BitManipulation.getBlockLightLevel
-
-  // // Get the Block state from raw Block data.
-  // // Parameters: [int] Raw block data.
-  // // Returns: [int] Block state.
-  // blockData.getState = BitManipulation.getBlockState
-
-  // // Set the Block ID into raw Block data.
-  // // Parameters: [int] Raw data [int] New Block ID.
-  // // Returns: [int] Manipulated raw Block data.
-  // blockData.setID = BitManipulation.setBlockID
-
-  // // Set the Block light level into raw Block data.
-  // // Parameters: [int] Raw data [int] New Block light level.
-  // // Returns: [int] Manipulated raw Block data.
-  // blockData.setLightLevel = BitManipulation.setBlockLightLevel
-
-  // // Set the Block state into raw Block data.
-  // // Parameters: [int] Raw data [int] New Block state.
-  // // Returns: [int] Manipulated raw Block data.
-  // blockData.setState = BitManipulation.setBlockState
-
-})();
-
-// Now globalize lua equivalents.
-const dofile = crafter.dofile;
-const loadstring = crafter.loadstring;
-const getFileString = crafter.getFileString;
-const math = crafter.math;
-
-// Kotlin equivalents.
-const println = print;
-
-//
-// Air is reserved here
-// crafter.registerBlock(
-//   new BlockDefinition("air")
-//     .setDrawType(DrawType.AIR)
-//     .setWalkable(false)
-//     .setReadableName("Air")
-//)
+crafter.registerBlock(0, "Air", "Air", ["air.png","air.png","air.png","air.png","air.png","air.png"], DrawType.AIR)
