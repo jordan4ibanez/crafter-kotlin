345a7543900e8df4f675e8751bc2997025e6ed10
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-05 06:08:38 -0400

    Clean this up

--- api/api.js
+++ api/api.js
@@ -1,26 +1,4 @@
 /*
- Crafter - A blocky game (engine) written in Java with LWJGL.
- Copyright (C) 2023  jordan4ibanez
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-/*
-TODO: Figure out more things like mobs & items & entities
-*/
-
-/*
 API is the base of the API which loads up class variables into the crafter array.
 This allows for mods to index DIRECTLY into the engine without needing to be compiled.
 Limitations: Ecmascript 6. Class architecture is not implemented into nashorn yet.
