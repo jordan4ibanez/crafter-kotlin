d9a35d165a2fb01bc63c25279863c286861cebd4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-06 05:02:01 -0400

    Add API setters

--- api/api.js
+++ api/api.js
@@ -111,11 +111,131 @@ const block = {
         // Then it gets injected here automatically
       }
     }
+  },
 
-  }
+  /**
+   * Set the inventory name of a block.
+   * @param {number | string} key ID or name.
+   * @param {string} newName 
+   */
+  setInventoryName: function(key, newName) {
+    jvmBlockController.setInventoryName(key, newName)
+  },
 
-}
 
+  /**
+   * Set the textures of a block.
+   * @param {number | string} key ID or name.
+   * @param {Array<string>} newTextures 
+   */
+  setTextures: function(key, newTextures) {
+    jvmBlockController.setTextures(key, newTextures)
+  },
+
+  /**
+   * Set the drawtype of a block.
+   * @param {number | string} key ID or name. 
+   * @param {DrawType} newDrawType 
+   */
+  setDrawType: function(key, newDrawType) {
+    jvmBlockController.setDrawType(key, newDrawType)
+  },
+
+  /**
+   * Set if a block is walkable.
+   * @param {number | string} key ID or name. 
+   * @param {boolean} isWalkable 
+   */
+  setWalkable: function(key, isWalkable) {
+    jvmBlockController.setWalkable(key, isWalkable)
+  },
+
+  /**
+   * Set if a block is liquid.
+   * @param {number | string} key ID or name.
+   * @param {boolean} isLiquid 
+   */
+  setLiquid: function(key, isLiquid) {
+    jvmBlockController.setLiquid(key, isLiquid)
+  },
+
+  /**
+   * Set the flow level of a block.
+   * @param {number | string} key ID or name. 
+   * @param {number} flowLevel Higher level makes liquid flow faster.
+   */
+  setFlow: function(key, flowLevel) {
+    jvmBlockController.setFlow(key, flowLevel)
+  },
+
+  /**
+   * Set the viscosity of a block.
+   * @param {number | string} key ID or name.
+   * @param {number} newViscosity Higher viscosity makes liquid harder to move through.
+   */
+  setViscosity: function(key, newViscosity) {
+    jvmBlockController.setViscosity(key, newViscosity)
+  },
+
+  /**
+   * Set if a block is climbable.
+   * @param {number | string} key ID or name.
+   * @param {number} isClimbable 
+   */
+  setClimbable: function(key, isClimbable) {
+    jvmBlockController.setClimbable(key, isClimbable)
+  },
+
+  /**
+   * Set if a block is sneak jump climbable.
+   * @param {number | string} key ID or name.
+   * @param {number} isSneakJumpClimbable 
+   */
+  setSneakJumpClimbable: function(key, isSneakJumpClimbable) {
+    jvmBlockController.setSneakJumpClimbable(key, isSneakJumpClimbable)
+  },
+
+  /**
+   * Set if a block is a falling block.
+   * @param {number | string} key ID or name.
+   * @param {boolean} isFalling 
+   */
+  setFalling: function(key, isFalling) {
+    jvmBlockController.setFalling(key, isFalling)
+  },
+
+  /**
+   * Set if a block is clear.
+   * @param {number | string} key 
+   * @param {boolean} isClear 
+   */
+  setClear: function(key, isClear) {
+    jvmBlockController.setClear(key, isClear)
+  },
+
+  /**
+   * Set a blocks damage per second.
+   * @param {number | string} key ID or name.
+   * @param {number} dps Damage Per Second.
+   */
+  setDamagePerSecond: function(key, dps) {
+    jvmBlockController.setDamagePerSecond(key, dps)
+  },
+
+  /**
+   * Set a blocks light level.
+   * @param {number | string} key ID or name.
+   * @param {*} newLight 
+   */
+  setLight: function(key, newLight) {
+    jvmBlockController.setLight(key, newLight)
+  },
+  
+
+};
+
+
+// Air is automatically registered here.
 block.register({
   name: "air",
   inventoryName: "air",
