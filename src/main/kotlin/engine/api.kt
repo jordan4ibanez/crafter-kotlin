270c22e79f6f580af1ed280d4b22b9d08585f0d5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-06 03:54:16 -0400

    Add more helpful crash info

--- src/main/kotlin/engine/api.kt
+++ src/main/kotlin/engine/api.kt
@@ -17,6 +17,7 @@ object api {
   private val bindings = javaScript.getBindings(ScriptContext.ENGINE_SCOPE)
   private val compiler = javaScript as Compilable
   private val invoker = javaScript as Invocable
+  private var currentFile = ""
 
   // One day
 //  private val tsCompiler = something
@@ -29,15 +30,20 @@ object api {
   }
 
   fun runFile(fileLocation: String) {
+    setCurrentFile(fileLocation)
     runCode(getFileString(fileLocation))
   }
 
   fun runCode(rawCode: String) {
-    try { javaScript.eval(rawCode) } catch (e: Exception) { throw RuntimeException("(Javascript API error):\n$e") }
+    try { javaScript.eval(rawCode) } catch (e: Exception) { throw RuntimeException("(Javascript API error in $currentFile):\n$e") }
   }
 
   private fun invoke(functionName: String, vararg args: Any): Any {
-    try { return invoker.invokeFunction(functionName, args) } catch (e: Exception) { throw RuntimeException("(Javascript API error):\n$e") }
+    try { return invoker.invokeFunction(functionName, args) } catch (e: Exception) { throw RuntimeException("(Javascript API error in $currentFile):\n$e") }
+  }
+
+  private fun setCurrentFile(fileLocation: String) {
+    currentFile = fileLocation
   }
 
   //note: These two probably aren't going to be used but I'm including them anyways just in case.
@@ -47,7 +53,7 @@ object api {
   }
 
   fun compileCode(rawCode: String): CompiledScript {
-    return try { compiler.compile(rawCode) } catch (e: Exception) { throw RuntimeException("(Javascript API error):\n$e") }
+    return try { compiler.compile(rawCode) } catch (e: Exception) { throw RuntimeException("(Javascript API error in $currentFile):\n$e") }
   }
 
 
