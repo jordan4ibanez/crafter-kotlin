d247377e368ef47b437f89fb483889a41af81f59
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-08 11:01:46 -0400

    Functional resolution of neighbor

--- src/main/kotlin/engine/world.kt
+++ src/main/kotlin/engine/world.kt
@@ -15,6 +15,7 @@ private const val DEPTH = 16
 private const val yStride = WIDTH * DEPTH
 private const val ARRAY_SIZE = WIDTH * HEIGHT * DEPTH
 
+private const val Y_SLICE_HEIGHT   = 16
 private const val MESH_ARRAY_SIZE = 8
 
 private var seed = 123_456_789
@@ -279,4 +280,99 @@ private fun fullBuildChunkMesh(posX: Int, posZ: Int, chunkData: IntArray) {
   val (frontExists, front) = safeGetDataDeconstruct(posX, posZ - 1)
 
 //  println("buildChunkMesh is running")
+
+  for (i in 0 until MESH_ARRAY_SIZE) {
+    buildMesh(
+      posX, i, posZ, chunkData,
+      leftExists, left,
+      rightExists, right,
+      backExists, back,
+      frontExists, front
+    )
+  }
+}
+
+private fun buildMesh(
+  posX: Int, posY: Int, posZ: Int, chunkData: IntArray,
+  leftExists: Boolean, leftChunk: IntArray,
+  rightExists: Boolean, rightChunk: IntArray,
+  backExists: Boolean, backChunk: IntArray,
+  frontExists: Boolean, frontChunk: IntArray
+) {
+
+  for (y in (Y_SLICE_HEIGHT * posY) until (Y_SLICE_HEIGHT * (posY + 1))) {
+    for (z in 0 until DEPTH) {
+      for (x in 0 until WIDTH) {
+        val currentBlock = chunkData[posToIndex(x,y,z)]
+        if (currentBlock.getBlockID() == 0) continue
+
+        val left = detectNeighbor(x,y,z,0,chunkData,leftExists,leftChunk)
+        val right = detectNeighbor(x,y,z,1,chunkData,rightExists,rightChunk)
+        val front = detectNeighbor(x,y,z,2,chunkData,frontExists,frontChunk)
+        val back = detectNeighbor(x,y,z,3,chunkData,backExists,backChunk)
+        val bottom = detectNeighbor(x,y,z,4, chunkData,false, IntArray(0))
+        val top = detectNeighbor(x,y,z,5, chunkData,false,IntArray(0))
+
+        when (block.getDrawType(currentBlock.getBlockID())) {
+          DrawType.AIR -> continue
+          DrawType.BLOCK -> {}
+          DrawType.BLOCK_BOX -> TODO()
+          DrawType.TORCH -> TODO()
+          DrawType.LIQUID_SOURCE -> TODO()
+          DrawType.LIQUID_FLOW -> TODO()
+          DrawType.GLASS -> TODO()
+          DrawType.PLANT -> TODO()
+          DrawType.LEAVES -> TODO()
+        }
+      }
+    }
+  }
+}
+
+private fun detectNeighbor(
+  x: Int, y: Int, z: Int,
+  // 0 left 1 right 2 front 3 back 4 bottom 5 top
+  dir: Int, chunkData: IntArray,
+  neighborExists: Boolean, neighbor: IntArray): Int {
+
+  val containmentCheck = when (dir) {
+    0 -> x - 1
+    1 -> x + 1
+    2 -> z - 1
+    3 -> z + 1
+    4 -> y - 1
+    5 -> y + 1
+    else -> throw RuntimeException("detectNeighbor: How 1??")
+  }
+  val containmentLimit = when (dir) {
+    0,1 -> WIDTH
+    2,3 -> DEPTH
+    4,5 -> HEIGHT
+    else -> throw RuntimeException("detectNeighbor: How 2??")
+  }
+
+  return if (!(0 until containmentLimit).contains(containmentCheck)) {
+    if (neighborExists) {
+      val index = when (dir) {
+        0 -> posToIndex(x + WIDTH,y,z)
+        1 -> posToIndex(x - WIDTH,y,z)
+        2 -> posToIndex(x,y,z + DEPTH)
+        3 -> posToIndex(x,y,z - DEPTH)
+        4,5 -> 0
+        else -> throw RuntimeException("detectNeighbor: How 3??")
+      }
+      neighbor[index]
+    } else { 0 }
+  } else {
+    val index = when (dir) {
+      0 -> posToIndex(x - 1,y,z)
+      1 -> posToIndex(x + 1,y,z)
+      2 -> posToIndex(x,y,z - 1)
+      3 -> posToIndex(x,y,z + 1)
+      4 -> posToIndex(x,y - 1, z)
+      5 -> posToIndex(x,y + 1, z)
+      else -> throw RuntimeException("detectNeighbor: How 4??")
+    }
+    chunkData[index]
+  }
 }
