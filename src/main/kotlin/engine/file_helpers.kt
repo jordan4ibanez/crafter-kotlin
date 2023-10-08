2b8fd4b208f64d617e90686dc70e04509ecf031d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-08 07:48:25 -0400

    Not all ideas are winners

--- src/main/kotlin/engine/file_helpers.kt
+++ src/main/kotlin/engine/file_helpers.kt
@@ -10,19 +10,16 @@ fun getFile(location: String): File {
   if (!file.exists()) throw RuntimeException("getFile: $location does not exist.")
   return file
 }
-fun String.getFile(): File = getFile(this)
 
 fun getFolder(location: String): File {
   val folder = File(location)
   if (!folder.isDirectory) throw RuntimeException("getFolder: $location is not a directory.")
   return folder
 }
-fun String.getFolder(): File = getFolder(this)
 
 fun getFileString(location: String): String {
   return getFile(location).readText()
 }
-fun String.getFileString(): String = getFileString(this)
 
 fun getFolderList(folderLocation: String): Array<String> {
   val folder = getFolder(folderLocation)
@@ -30,7 +27,6 @@ fun getFolderList(folderLocation: String): Array<String> {
     File(currentFolder, name).isDirectory
   }!!
 }
-fun String.getFolderList(): Array<String> = getFolderList(this)
 
 fun getFileList(folderLocation: String): Array<String> {
   val folder = getFolder(folderLocation)
@@ -38,24 +34,19 @@ fun getFileList(folderLocation: String): Array<String> {
     File(currentFile, name).isFile
   }!!
 }
-fun String.getFileList(): Array<String> = getFileList(this)
 
 fun isFolder(folderLocation: String): Boolean {
   return File(folderLocation).isDirectory
 }
-fun String.isFolder(): Boolean = isFolder(this)
 
 fun isFile(fileLocation: String): Boolean {
   return File(fileLocation).isFile
 }
-fun String.isFile(): Boolean = isFile(this)
 
 fun makeFolder(folderLocation: String): Boolean {
   return File(folderLocation).mkdir()
 }
-fun String.makeFolder(): Boolean = makeFolder(this)
 
 fun makeFile(fileLocation: String): File {
   return File(fileLocation)
 }
-fun String.makeFile(): File = makeFile(this)
