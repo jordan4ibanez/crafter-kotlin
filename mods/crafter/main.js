db6a76cbbe2e2f6f679c4d2c42d1894d78999715
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-08 08:49:43 -0400

    Clean up crafter's main file

--- mods/crafter/main.js
+++ mods/crafter/main.js
@@ -1,9 +1,9 @@
 // * You have to import to get JSDoc working.
 import("../../api/api")
 
-const modDir = crafter.getCurrentModDirectory() + "/";
+const modDir = crafter.getCurrentModDirectory();
 
 // Load up all the separate mod files.
 for (const modFile of ["blocks", "biomes", "scriptingTest"]) {
-  dofile(modDir + modFile + ".js");
+  dofile(`${modDir}/${modFile}.js`);
 }
