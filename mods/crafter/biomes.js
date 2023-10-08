7f47f4647b6f0fcabe2bd62239243bb0ad299570
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-08 10:06:10 -0400

    Take a different approach

--- mods/crafter/biomes.js
+++ mods/crafter/biomes.js
@@ -2,13 +2,13 @@ import("../../api/api")
 
 // Biome registration
 
-// crafter.registerBiome(
-//     new BiomeDefinition("classic")
-//         .setScale(20.0)
-//         .setFrequency(0.01)
-//         .setGrassLayer("crafter:grass")
-//         .setDirtLayer("crafter:dirt")
-//         .setStoneLayer("crafter:stone")
-//         // ore definition goes here
-//         // cave noise definition goes here
-// );
+crafter.registerBiome(
+    new BiomeDefinition("classic")
+        .setScale(20.0)
+        .setFrequency(0.01)
+        .setGrassLayer("crafter:grass")
+        .setDirtLayer("crafter:dirt")
+        .setStoneLayer("crafter:stone")
+        // ore definition goes here
+        // cave noise definition goes here
+);
