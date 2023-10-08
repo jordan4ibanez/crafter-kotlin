df8e906017613ab24bf7d9a207d3deb35e5818bc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-08 10:04:26 -0400

    Translate block registration to new API

--- mods/crafter/blocks.js
+++ mods/crafter/blocks.js
@@ -4,47 +4,47 @@ import("../../api/api")
 block.register({
   name: "crafter:stone",
   inventoryName: "Stone",
-  textures: ["stone.png","stone.png","stone.png","stone.png","stone.png","stone.png"]
+  textures: ["stone.png","stone.png","stone.png","stone.png","stone.png","stone.png"],
 });
 
-// crafter.registerBlock(
-//     new BlockDefinition("crafter:dirt")
-//         .setReadableName("Dirt")
-//         .setTextures(["dirt.png","dirt.png","dirt.png","dirt.png","dirt.png","dirt.png"])
-// );
+block.register({
+  name: "crafter:dirt",
+  inventoryName: "Dirt",
+  textures: ["dirt.png","dirt.png","dirt.png","dirt.png","dirt.png","dirt.png"],
+});
 
-// crafter.registerBlock(
-//     new BlockDefinition("crafter:grass")
-//         .setReadableName("Grass")
-//         .setTextures(["grass.png","grass.png","grass.png","grass.png","dirt.png","grass.png"])
-// );
+block.register({
+  name: "crafter:grass",
+  inventoryName: "Grass",
+  textures: ["grass.png","grass.png","grass.png","grass.png","dirt.png","grass.png"],
+});
 
-// crafter.registerBlock(
-//     new BlockDefinition("crafter:tree")
-//         .setReadableName("Tree")
-//         .setTextures(["treeOut.png","treeOut.png","treeOut.png","treeOut.png","treeCore.png","treeCore.png"])
-// );
+block.register({
+  name: "crafter:tree",
+  inventoryName: "Tree",
+  textures: ["treeOut.png","treeOut.png","treeOut.png","treeOut.png","treeCore.png","treeCore.png"],
+});
 
-// crafter.registerBlock(
-//     new BlockDefinition("crafter:wood")
-//         .setReadableName("Wood")
-//         .setTextures(["wood.png","wood.png","wood.png","wood.png","wood.png","wood.png"])
-// );
+block.register({
+  name: "crafter:wood",
+  inventoryName: "Wood",
+  textures: ["wood.png","wood.png","wood.png","wood.png","wood.png","wood.png"],
+});
 
-// crafter.registerBlock(
-//     new BlockDefinition("crafter:tnt")
-//         .setReadableName("TNT")
-//         .setTextures(["tnt_side.png","tnt_side.png","tnt_side.png","tnt_side.png","tnt_bottom.png","tnt_top.png"])
-// );
+block.register({
+  name: "crafter:tnt",
+  inventoryName: "TNT",
+  textures: ["tnt_side.png","tnt_side.png","tnt_side.png","tnt_side.png","tnt_bottom.png","tnt_top.png"],
+});
 
-// crafter.registerBlock(
-//     new BlockDefinition("crafter:cobblestone")
-//         .setReadableName("Cobblestone")
-//         .setTextures(["cobble.png","cobble.png","cobble.png","cobble.png","cobble.png","cobble.png"])
-// );
+block.register({
+  name: "crafter:cobblestone",
+  inventoryName: "Cobblestone",
+  textures: ["cobble.png","cobble.png","cobble.png","cobble.png","cobble.png","cobble.png"],
+});
 
-// crafter.registerBlock(
-//     new BlockDefinition("crafter:brick")
-//         .setReadableName("Brick")
-//         .setTextures(["brick.png","brick.png","brick.png","brick.png","brick.png","brick.png"])
-// );
+block.register({
+  name: "crafter:brick",
+  inventoryName: "Brick",
+  textures: ["brick.png","brick.png","brick.png","brick.png","brick.png","brick.png"],
+});
