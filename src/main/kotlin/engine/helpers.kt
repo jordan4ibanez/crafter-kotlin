9e6545198575ada7d23ed8cda5b0276966e6bffe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-15 23:49:17 -0400

    Add mutable iterators thanks to kimapr

---
+++ src/main/kotlin/engine/helpers.kt
@@ -0,0 +1,50 @@
+package engine
+
+// Thanks, kimapr! This will be quite useful. https://stackoverflow.com/a/34608552
+inline fun <T> Array<T>.mapInPlace(mutator: (T) -> T) {
+  this.forEachIndexed { idx, value ->
+    mutator(value).let { newValue ->
+      if (newValue !== value) this[idx] = mutator(value)
+    }
+  }
+}
+
+inline fun <T> Array<T>.mapInPlaceIndexed(mutator: (Int, T) -> T) {
+  this.forEachIndexed { idx, value ->
+    mutator(idx, value).let { newValue ->
+      if (newValue !== value) this[idx] = mutator(idx, value)
+    }
+  }
+}
+
+inline fun IntArray.mapInPlace(mutator: (Int) -> Int) {
+  this.forEachIndexed { idx, value ->
+    mutator(value).let { newValue ->
+      if (newValue != value) this[idx] = mutator(value)
+    }
+  }
+}
+
+inline fun IntArray.mapInPlaceIndexed(mutator: (Int, Int) -> Int) {
+  this.forEachIndexed { idx, value ->
+    mutator(idx, value).let { newValue ->
+      if (newValue != value) this[idx] = mutator(idx, value)
+    }
+  }
+}
+
+inline fun FloatArray.mapInPlace(mutator: (Float) -> Float) {
+  this.forEachIndexed { idx, value ->
+    mutator(value).let { newValue ->
+      if (newValue != value) this[idx] = mutator(value)
+    }
+  }
+}
+
+inline fun FloatArray.mapInPlaceIndexed(mutator: (Int, Float) -> Float) {
+  this.forEachIndexed { idx, value ->
+    mutator(idx, value).let { newValue ->
+      if (newValue != value) this[idx] = mutator(idx, value)
+    }
+  }
+}
