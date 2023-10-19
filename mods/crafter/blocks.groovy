bff4974853ac65d19f6945dc3e618e7a23d1ed53
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-19 05:40:11 -0400

    And the rest

--- mods/crafter/blocks.groovy
+++ mods/crafter/blocks.groovy
@@ -1,6 +1,5 @@
 package crafter
 
-import engine.DrawType
 import engine.block
 import engine.api
 
@@ -26,3 +25,34 @@ block.register(
   inventoryName = "Grass",
   textures = api.stringArrayOf("grass.png","grass.png","grass.png","grass.png","dirt.png","grass.png"),
 )
+
+block.register(
+  name = "crafter:tree",
+  inventoryName = "Tree",
+  textures = api.stringArrayOf("treeOut.png","treeOut.png","treeOut.png","treeOut.png","treeCore.png","treeCore.png"),
+)
+
+block.register(
+  name = "crafter:wood",
+  inventoryName = "Wood",
+  textures = api.stringArrayOf("wood.png","wood.png","wood.png","wood.png","wood.png","wood.png"),
+)
+
+block.register(
+  name = "crafter:tnt",
+  inventoryName = "TNT",
+  textures = api.stringArrayOf("tnt_side.png","tnt_side.png","tnt_side.png","tnt_side.png","tnt_bottom.png","tnt_top.png"),
+)
+
+block.register(
+  name = "crafter:cobblestone",
+  inventoryName = "Cobblestone",
+  textures = api.stringArrayOf("cobble.png","cobble.png","cobble.png","cobble.png","cobble.png","cobble.png"),
+)
+
+block.register(
+  name = "crafter:brick",
+  inventoryName = "Brick",
+  textures = api.stringArrayOf("brick.png","brick.png","brick.png","brick.png","brick.png","brick.png"),
+)
+
