07f8f35f0b5a44f06ddfc3c0d04d18fc2c530866
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-19 05:43:50 -0400

    Clean this up a bit

--- mods/crafter/main.groovy
+++ mods/crafter/main.groovy
@@ -1,11 +1,8 @@
 package crafter
 
-import org.joml.Vector3f
 import engine.api
 
 final api = api.INSTANCE
-
-final String currentDir = api.getCurrentModDirectory()
 
 ["blocks"].forEach {
     api.dofile("crafter/$it")
