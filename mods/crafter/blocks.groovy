e2d7ec95de39ea1789fbf496a93c6915ab0756b6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-19 05:35:29 -0400

    Now register the classics

--- mods/crafter/blocks.groovy
+++ mods/crafter/blocks.groovy
@@ -2,7 +2,27 @@ package crafter
 
 import engine.DrawType
 import engine.block
+import engine.api
 
+final api = api.INSTANCE
 final block = block.INSTANCE
 
-block.register("test", "test", new String[]{"test"}, DrawType.BLOCK)
+
+
+block.register(
+  name = "crafter:stone",
+  inventoryName = "Stone",
+  textures = api.stringArrayOf("stone.png","stone.png","stone.png","stone.png","stone.png","stone.png")
+)
+
+block.register(
+  name = "crafter:dirt",
+  inventoryName = "Dirt",
+  textures = api.stringArrayOf("dirt.png","dirt.png","dirt.png","dirt.png","dirt.png","dirt.png"),
+)
+
+block.register(
+  name = "crafter:grass",
+  inventoryName = "Grass",
+  textures = api.stringArrayOf("grass.png","grass.png","grass.png","grass.png","dirt.png","grass.png"),
+)
