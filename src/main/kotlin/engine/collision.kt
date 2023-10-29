d5136b8914b4e20c9e3507ce4aab0141d2dfcb20
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-29 03:19:54 -0400

    Basic basics

--- src/main/kotlin/engine/collision.kt
+++ src/main/kotlin/engine/collision.kt
@@ -3,6 +3,7 @@ package engine
 import org.joml.FrustumIntersection
 import org.joml.Matrix4f
 import org.joml.Vector3f
+import org.joml.Vector3fc
 
 object collision {
   private val chunkMatrix = Matrix4f()
@@ -11,13 +12,27 @@ object collision {
   private val min = Vector3f()
   private val max = Vector3f()
   private val gravity = world.getGravity()
+  private var accumulator = 0f
+  private val oldPos = Vector3f()
 
   //? note: Entity collision.
-  
-  internal fun collideEntityToWorld(entity: GroovyEntity) {
+
+  internal fun collideEntityToWorld(entity: GroovyEntity, delta: Float) {
     val size = entity.getSize()
     val pos = entity.getPosition()
     val velocity = entity.getVelocity()
+
+    // If this entity exists in an area that's unloaded, freeze.
+    if (!blockManipulator.set(
+      pos.x() - size.x(), pos.y(), pos.z() - size.x(),
+      pos.x() + size.x(), pos.y() + size.y(), pos.z() + size.x())) return
+
+    oldPos.set(pos)
+
+
+    blockManipulator.forEach {
+
+    }
 
 
   }
