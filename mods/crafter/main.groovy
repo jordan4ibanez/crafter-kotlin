9cb5c482d04799878c0da8a43dee1e8fd914a5b5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-29 02:25:18 -0400

    Now disable test

--- mods/crafter/main.groovy
+++ mods/crafter/main.groovy
@@ -4,9 +4,9 @@ import engine.api
 
 final api = api.INSTANCE
 
-api.registerOnTick { float delta ->
-  println("delta is $delta")
-}
+//api.registerOnTick { float delta ->
+//  println("delta is $delta")
+//}
 
 ["blocks", "entities"].forEach {
   api.dofile("crafter/$it")
