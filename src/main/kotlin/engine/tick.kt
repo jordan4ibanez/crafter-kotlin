385b6ffb6eb14759515dfa90b367cfdd4456647e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-29 04:57:43 -0400

    Tick that thinks

---
+++ src/main/kotlin/engine/tick.kt
@@ -0,0 +1,15 @@
+package engine
+
+object tick {
+  private var accumulator = 0f
+  private const val GOAL = 20
+  internal const val DELTA = 1f / GOAL.toFloat()
+  internal fun think(delta: Float): Boolean {
+    accumulator += delta
+    if (accumulator >= DELTA) {
+      accumulator -= DELTA
+      return true
+    }
+    return false
+  }
+}
