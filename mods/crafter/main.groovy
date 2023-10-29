8e3928b586528ac611bda125b5925700510ea2e9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-29 02:21:35 -0400

    Now add very basic onTick

--- mods/crafter/main.groovy
+++ mods/crafter/main.groovy
@@ -4,6 +4,10 @@ import engine.api
 
 final api = api.INSTANCE
 
+api.registerOnTick { float delta ->
+  println("delta is $delta")
+}
+
 ["blocks", "entities"].forEach {
-    api.dofile("crafter/$it")
+  api.dofile("crafter/$it")
 }
