128fb295575417b4b5af2e2b2b02491b6380e2a4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-31 08:12:39 -0400

    Re-tune friction

--- src/main/kotlin/engine/block.kt
+++ src/main/kotlin/engine/block.kt
@@ -226,7 +226,7 @@ object block {
 
   // ID oriented.
   fun getFriction(id: Int): Float {
-    return friction[id] ?: 1.325f
+    return friction[id] ?: 0.8f
   }
   fun isWalkable(id: Int): Boolean {
     return walkable[id] ?: true
