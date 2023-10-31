036c99818f3dac5613ebc0bb25fe8d8a98d4ba61
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-10-31 03:34:34 -0400

    Make an ice payload

--- mods/crafter/main.groovy
+++ mods/crafter/main.groovy
@@ -1,12 +1,41 @@
 package crafter
 
 import engine.api
+import engine.blockManipulator
+import org.joml.Vector3f
+import org.joml.Vector3fc
+import org.joml.Vector3i
+import org.joml.Vector3ic
+import engine.block
 
 final api = api.INSTANCE
+final blockManipulator = blockManipulator.INSTANCE
+final block = block.INSTANCE
 
-//api.registerOnTick { float delta ->
-//  println("delta is $delta")
-//}
+boolean payloaded = false
+
+api.registerOnTick { float delta ->
+  if (!payloaded) {
+    final int airID = block.getID("air")
+    final int iceID = block.getID("crafter:ice")
+    final Vector3ic min = new Vector3i(-31,0,-31)
+    final Vector3ic max = new Vector3i(32, 127, 32)
+    if (blockManipulator.set(min, max)) {
+      println("it wurk")
+      payloaded = true
+      for (x in min.x() .. max.x()) {
+        for (z in min.z() .. max.z()) {
+          for (y in min.y() .. max.y()) {
+            if (blockManipulator.getID(x,y,z) != airID) {
+              blockManipulator.setID(x,y,z, iceID)
+            }
+          }
+        }
+      }
+      blockManipulator.write()
+    }
+  }
+}
 
 ["blocks", "entities"].forEach {
   api.dofile("crafter/$it")
