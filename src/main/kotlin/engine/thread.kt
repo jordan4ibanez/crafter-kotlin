20d5cc1fb2fa30e637a15ffd227c1f33c37dea4b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-01 16:43:31 -0400

    Add a whole bunch of work

--- src/main/kotlin/engine/thread.kt
+++ src/main/kotlin/engine/thread.kt
@@ -1,11 +1,7 @@
 package engine
 
-import kotlinx.coroutines.asCoroutineDispatcher
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.Executor
+import kotlinx.coroutines.Runnable
 import java.util.concurrent.Executors
-import kotlin.math.max
-import kotlin.math.min
 
 // All functions which launch coroutines go into here.
 
@@ -13,42 +9,37 @@ object thread {
   // -1 because main thread.
   private val availableCores = Runtime.getRuntime().availableProcessors() - 1
   // Cached is better for servers.
-  private val executor = Executors.newCachedThreadPool()//Executors.newFixedThreadPool(availableCores)
+  private val executor = Executors.newFixedThreadPool(availableCores)
 
+//  fun formatSize(v: Long): String {
+//    if (v < 1024) return "$v B"
+//    val z = (63 - java.lang.Long.numberOfLeadingZeros(v)) / 10
+//    return String.format("%.1f %sB", v.toDouble() / (1L shl z * 10), " KMGTPE"[z])
+//  }
   internal fun launchAllThreads() {
+    val runtime = Runtime.getRuntime()
+//    val heapSize = runtime.totalMemory()
+//    val maxHeap = runtime.maxMemory()
+//    val free = runtime.freeMemory()
+//    println("heap: ${formatSize(heapSize)} | max: ${formatSize(maxHeap)} | free: ${formatSize(free)}")
     world.disperseChunkGenerators()
   }
   internal fun launch(work: Runnable) {
-//    println(Thread.activeCount())
+//    println("active threads: ${Thread.activeCount()}")
     executor.execute(work)
   }
   fun getCPUCores() = availableCores
 
   fun IntRange.parallelForEach(work: (Int) -> Unit) {
     if (this.step != 1) throw RuntimeException("Does not work for skip ranges.")
-    val size = max(this.first, this.last) - min(this.first, this.last)
-    val rangeSize = size / availableCores
+    val size = this.last - this.first
+    val rangeSize = if (size / availableCores == 0) availableCores else size / availableCores
     val remainder = size % availableCores
     (0 until availableCores).forEach { i ->
       val start = this.first + (i * rangeSize)
-      val end =  if (i == (availableCores - 1))  (start + rangeSize) + remainder else (start + rangeSize) - 1
-      launch {
-        (start .. end).forEach(work)
-      }
+      if (start > this.last) return
+      val end = if (i == availableCores - 1) (start + rangeSize + remainder) else ((start + rangeSize) - 1)
+      launch { (start..end).forEach(work) }
     }
-  }
-
-  fun <K, V> ConcurrentHashMap<K, V>.parallelForEach(work: (Int) -> Unit) {
-    if (this.step != 1) throw RuntimeException("Does not work for skip ranges.")
-  }
-
-   fun <K, V> Map<out K, V>.forEach(work: (Map.Entry<K, V>) -> Unit) {
-     val size = this.size
-     val rangeSize = size / availableCores
-     val remainder = size % availableCores
-
-     (0 until availableCores).forEach { i ->
-       this.keys.
-     }
   }
 }
