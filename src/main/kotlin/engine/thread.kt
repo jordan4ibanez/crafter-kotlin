397b50829f1fd30c726246782c57c608646e9362
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-01 12:20:23 -0400

    Dump kotlin coroutines for thread pool

--- src/main/kotlin/engine/thread.kt
+++ src/main/kotlin/engine/thread.kt
@@ -1,7 +1,17 @@
 package engine
 
-// All functions which launch coroutines go into here.
-fun launchAllThreads() {
-  world.disperseChunkGenerators()
+import kotlinx.coroutines.asCoroutineDispatcher
+import java.util.concurrent.Executors
 
+// All functions which launch coroutines go into here.
+
+object thread {
+  // -1 because main thread.
+  private val executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 1)
+  internal fun launchAllThreads() {
+    world.disperseChunkGenerators()
+  }
+  fun launch(work: Runnable) {
+    executor.execute(work)
+  }
 }
