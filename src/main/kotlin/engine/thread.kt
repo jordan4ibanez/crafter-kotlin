6d22ead85ed3e0c5add2bde7170915d55319a38d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-03 12:31:25 -0400

    Simplify how parallel for each works

--- src/main/kotlin/engine/thread.kt
+++ src/main/kotlin/engine/thread.kt
@@ -1,6 +1,5 @@
 package engine
 
-import kotlinx.coroutines.Runnable
 import java.util.concurrent.Executors
 
 // All functions which launch coroutines go into here.
@@ -15,7 +14,7 @@ object thread {
   // note: runs out of control
 //  private val executor = Executors.newCachedThreadPool()
 
-//  fun formatSize(v: Long): String {
+  //  fun formatSize(v: Long): String {
 //    if (v < 1024) return "$v B"
 //    val z = (63 - java.lang.Long.numberOfLeadingZeros(v)) / 10
 //    return String.format("%.1f %sB", v.toDouble() / (1L shl z * 10), " KMGTPE"[z])
@@ -28,24 +27,17 @@ object thread {
 //    println("heap: ${formatSize(heapSize)} | max: ${formatSize(maxHeap)} | free: ${formatSize(free)}")
     world.disperseChunkGenerators()
   }
+
   internal fun launch(work: Runnable) {
 //    println("active threads: ${Thread.activeCount()}")
     executor.execute(work)
   }
+
   fun getCPUCores() = availableCores
 
-  fun IntRange.parallelForEach(work: (Int) -> Unit) {
-    if (this.step != 1) throw RuntimeException("Does not work for skip ranges.")
-    val size = this.last - this.first
-    val rangeSize = if (size / availableCores == 0) availableCores else size / availableCores
-    val remainder = size % availableCores
-    (0 until availableCores).forEach { i ->
-      val start = this.first + (i * rangeSize)
-      if (start > this.last) return
-      val end = if (i == availableCores - 1) (start + rangeSize + remainder) else ((start + rangeSize) - 1)
-      launch { (start..end).forEach(work) }
-    }
-  }
+
+  fun IntRange.parallelForEach(work: (Int) -> Unit) = launch { this.forEach(work) }
+
 
   internal fun destroy() {
     executor.shutdownNow()
