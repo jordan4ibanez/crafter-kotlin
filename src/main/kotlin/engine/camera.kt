02328424b31dccc75e25b5d6819be4c4ce2ec8a9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-03 12:30:22 -0400

    render 64

--- src/main/kotlin/engine/camera.kt
+++ src/main/kotlin/engine/camera.kt
@@ -13,7 +13,7 @@ import org.lwjgl.system.windows.INPUT
 
 object camera {
 
-  private var renderDistance = 8
+  private var renderDistance = 64
 
   private var sensitivity = 1f / 500f
   private var FOV: Float = toRadians(60f)
