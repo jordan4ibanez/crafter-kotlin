186bf0586d9c5c73bcbc53e433b800c561f2b353
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-19 12:23:21 -0400

    This one too

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,13 @@
-
-//dependencyResolutionManagement {
-//    repositories {
-//        mavenCentral()
-//        gradlePluginPortal()
-//    }
-//}
-
 pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
+  repositories {
+    mavenCentral()
+    gradlePluginPortal()
+  }
 }
 
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
 rootProject.name = "crafter"
