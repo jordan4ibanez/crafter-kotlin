d3422832fe94c3ee06c74fe5605fc2590809f9ac
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-19 09:46:41 -0400

    Update main.groovy

--- mods/crafter/main.groovy
+++ mods/crafter/main.groovy
@@ -1,12 +1,10 @@
 package crafter
 
-import engine.api
+import engine.api.api
+import engine.block
 import engine.blockManipulator
-import org.joml.Vector3f
-import org.joml.Vector3fc
 import org.joml.Vector3i
 import org.joml.Vector3ic
-import engine.block
 
 final api = api.INSTANCE
 final blockManipulator = blockManipulator.INSTANCE
@@ -15,28 +13,28 @@ final block = block.INSTANCE
 boolean payloaded = false
 
 api.registerOnTick { float delta ->
-  if (!payloaded) {
-    final int airID = block.getID("air")
-    final int iceID = block.getID("crafter:ice")
-    final Vector3ic min = new Vector3i(-31,0,-31)
-    final Vector3ic max = new Vector3i(32, 127, 32)
-    if (blockManipulator.set(min, max)) {
+    if (!payloaded) {
+        final int airID = block.getID("air")
+        final int iceID = block.getID("crafter:ice")
+        final Vector3ic min = new Vector3i(-31, 0, -31)
+        final Vector3ic max = new Vector3i(32, 127, 32)
+        if (blockManipulator.set(min, max)) {
 //      println("it wurk")
-      payloaded = true
-      for (x in min.x() .. max.x()) {
-        for (z in min.z() .. max.z()) {
-          for (y in min.y() .. max.y()) {
-            if (blockManipulator.getID(x,y,z) != airID) {
-              blockManipulator.setID(x,y,z, iceID)
+            payloaded = true
+            for (x in min.x()..max.x()) {
+                for (z in min.z()..max.z()) {
+                    for (y in min.y()..max.y()) {
+                        if (blockManipulator.getID(x, y, z) != airID) {
+                            blockManipulator.setID(x, y, z, iceID)
+                        }
+                    }
+                }
             }
-          }
+            blockManipulator.write()
         }
-      }
-      blockManipulator.write()
     }
-  }
 }
 
 ["blocks", "entities"].forEach {
-  api.dofile("crafter/$it")
+    api.dofile("crafter/$it")
 }
