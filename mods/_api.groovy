4e52673df37e63c0aa34fb60ab22360072756c20
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-19 09:46:24 -0400

    Update _api.groovy

--- mods/_api.groovy
+++ mods/_api.groovy
@@ -1,6 +1,6 @@
 import engine.DrawType
+import engine.api.api
 import engine.block
-import engine.api
 
 final block = block.INSTANCE
 final api = api.INSTANCE
@@ -9,9 +9,9 @@ println("Crafter API is feeling pretty groovy. 8)")
 
 // Air is registered here.
 block.register(
-  name = "air",
-  inventoryName = "air",
-  textures = api.stringArrayOf("air.png","air.png","air.png","air.png","air.png","air.png"),
-  drawtype = DrawType.AIR
+        name = "air",
+        inventoryName = "air",
+        textures = api.stringArrayOf("air.png", "air.png", "air.png", "air.png", "air.png", "air.png"),
+        drawtype = DrawType.AIR
 )
 block.setWalkable("air", false)
