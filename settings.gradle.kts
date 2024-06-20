4efb74d25bf25f90ca7b0800bd71be8ecc28f822
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-20 05:45:39 -0400

    Upgrade this

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,12 +4,8 @@ pluginManagement {
     gradlePluginPortal()
   }
 }
-
-
 plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-
 rootProject.name = "crafter"
-
-include("app", "core")
+include("src")
