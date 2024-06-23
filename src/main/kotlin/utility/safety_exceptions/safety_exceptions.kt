3dbfbba6f17a9318852e6de5c5c70816085e8e26
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-23 06:22:00 -0400

    Time for some overhaul

--- src/main/kotlin/utility/safety_exceptions/safety_exceptions.kt
+++ src/main/kotlin/utility/safety_exceptions/safety_exceptions.kt
@@ -9,3 +9,13 @@ class UnwrapException(info: String) : Exception(info)
  * Exception class which indicates that something was expected, but contained nothing.
  */
 class ExpectException(info: String) : Exception(info)
+
+/**
+ * Exception class which indicates that a OneLock was set more than once.
+ */
+class MultipleOneLockSetException(info: String) : Exception(info)
+
+/**
+ * Exception class which indicates we have literally no idea what you did.
+ */
+class UnknownException(info: String) : Exception(info)
