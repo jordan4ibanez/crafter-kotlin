7353e8c0291965121d70d149adf30c3a5c4abc5b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-24 03:39:24 -0400

    Create OneLockTest.kt

---
+++ src/test/kotlin/utility/onelock/OneLockTest.kt
@@ -0,0 +1,16 @@
+package utility.onelock
+
+import org.junit.jupiter.api.Test
+
+class OneLockTest {
+
+  @Test
+  fun introduction() {
+    println("=== Starting OneLock Test ===")
+  }
+
+  @Test
+  fun conclusion() {
+    println("=== OneLock Test Concluded ===")
+  }
+}
