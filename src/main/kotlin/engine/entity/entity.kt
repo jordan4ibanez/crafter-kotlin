78fa7bbba5163e7a58ae7d73eda3406569fc0a49
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-27 09:04:42 -0400

    Make PointEntity it's own thing

--- src/main/kotlin/engine/entity/entity.kt
+++ src/main/kotlin/engine/entity/entity.kt
@@ -2,9 +2,9 @@ package engine.entity
 
 import engine.collision.collision
 import engine.collision_box.collisionBox
+import engine.entity.point_entity.PointEntity
 import engine.joml_bolt_ons.print
 import engine.tick.tick
-import engine.world.world
 import org.joml.Vector2f
 import org.joml.Vector2fc
 import org.joml.Vector3f
@@ -12,7 +12,7 @@ import org.joml.Vector3fc
 import java.util.*
 
 const val interpolationSnappiness = tick.GOAL
-private val vector3Worker = Vector3f()
+internal val vector3Worker = Vector3f()
 private val vector2Worker = Vector2f()
 
 private val vel2d = Vector2f()
@@ -20,65 +20,6 @@ private val diff = Vector2f()
 private val accelerationWorker = Vector3f()
 private val goalVel = Vector2f()
 
-open class PointEntity {
-
-  internal var interpolationTimer = 0f
-  internal val oldPosition = Vector3f()
-  internal val interpolationPosition = Vector3f()
-  val position = Vector3f()
-  private val velocity = Vector3f()
-  private val acceleration = Vector3f(0f, -world.getGravity(), 0f)
-  var meshID = 0
-  var onGround = false
-  internal var friction = 0.8f
-
-  constructor(pos: Vector3fc) {
-    this.position.set(pos)
-    oldPosition.set(pos)
-    interpolationPosition.set(pos)
-  }
-
-  fun getFriction(): Float = friction
-
-  fun getPosition(): Vector3fc = position
-  open fun setPosition(newPosition: Vector3fc) {
-    oldPosition.set(position)
-    position.set(newPosition)
-    interpolationTimer = 0f
-  }
-
-  internal fun interpolate(delta: Float) {
-    if (interpolationTimer >= 1f) {
-//      println("interpolation is complete $interpolationTimer")
-      return
-    }
-//    println("interpolating thing $interpolationTimer")
-    interpolationTimer += delta * interpolationSnappiness
-//    println("interpolation reached $interpolationTimer")
-    if (interpolationTimer >= 1f) {
-//      println("interpolation completed")
-      interpolationTimer = 1f
-    }
-    oldPosition.lerp(position, interpolationTimer, interpolationPosition)
-  }
-
-  fun getVelocity(): Vector3fc = velocity
-  fun setVelocity(x: Float, y: Float, z: Float) = setVelocity(vector3Worker.set(x, y, z))
-  open fun setVelocity(newVelocity: Vector3fc) {
-    velocity.set(newVelocity)
-  }
-
-  fun addVelocity(moreVelocity: Vector3fc) = addVelocity(moreVelocity.x(), moreVelocity.y(), moreVelocity.z())
-  fun addVelocity(x: Float, y: Float, z: Float) = velocity.add(x, y, z)
-
-  fun getAcceleration(): Vector3fc = acceleration
-  fun setAcceleration(x: Float, y: Float, z: Float) = setAcceleration(vector3Worker.set(x, y, z))
-  open fun setAcceleration(newAcceleration: Vector3fc) {
-    acceleration.set(newAcceleration)
-  }
-
-  open fun onTick(delta: Float) {}
-}
 
 class Particle : PointEntity {
   constructor(pos: Vector3fc) : super(pos)
---
+++ src/main/kotlin/engine/entity/point_entity/PointEntity.kt
@@ -0,0 +1,71 @@
+package engine.entity.point_entity
+
+import engine.entity.interpolationSnappiness
+import engine.world.world
+import org.joml.Vector2f
+import org.joml.Vector3f
+import org.joml.Vector3fc
+
+internal val vector3Worker = Vector3f()
+private val vector2Worker = Vector2f()
+
+
+open class PointEntity {
+
+  internal var interpolationTimer = 0f
+  internal val oldPosition = Vector3f()
+  internal val interpolationPosition = Vector3f()
+  val position = Vector3f()
+  private val velocity = Vector3f()
+  private val acceleration = Vector3f(0f, -world.getGravity(), 0f)
+  var meshID = 0
+  var onGround = false
+  internal var friction = 0.8f
+
+  constructor(pos: Vector3fc) {
+    this.position.set(pos)
+    oldPosition.set(pos)
+    interpolationPosition.set(pos)
+  }
+
+  fun getFriction(): Float = friction
+
+  fun getPosition(): Vector3fc = position
+  open fun setPosition(newPosition: Vector3fc) {
+    oldPosition.set(position)
+    position.set(newPosition)
+    interpolationTimer = 0f
+  }
+
+  internal fun interpolate(delta: Float) {
+    if (interpolationTimer >= 1f) {
+//      println("interpolation is complete $interpolationTimer")
+      return
+    }
+//    println("interpolating thing $interpolationTimer")
+    interpolationTimer += delta * interpolationSnappiness
+//    println("interpolation reached $interpolationTimer")
+    if (interpolationTimer >= 1f) {
+//      println("interpolation completed")
+      interpolationTimer = 1f
+    }
+    oldPosition.lerp(position, interpolationTimer, interpolationPosition)
+  }
+
+  fun getVelocity(): Vector3fc = velocity
+  fun setVelocity(x: Float, y: Float, z: Float) = setVelocity(vector3Worker.set(x, y, z))
+  open fun setVelocity(newVelocity: Vector3fc) {
+    velocity.set(newVelocity)
+  }
+
+  fun addVelocity(moreVelocity: Vector3fc) = addVelocity(moreVelocity.x(), moreVelocity.y(), moreVelocity.z())
+  fun addVelocity(x: Float, y: Float, z: Float) = velocity.add(x, y, z)
+
+  fun getAcceleration(): Vector3fc = acceleration
+  fun setAcceleration(x: Float, y: Float, z: Float) = setAcceleration(vector3Worker.set(x, y, z))
+  open fun setAcceleration(newAcceleration: Vector3fc) {
+    acceleration.set(newAcceleration)
+  }
+
+  open fun onTick(delta: Float) {}
+}
