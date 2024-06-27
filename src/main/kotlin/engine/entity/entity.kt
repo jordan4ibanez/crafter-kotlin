b245c13af2dafc809d3fa0c6f4d5d7d8cda99402
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-06-27 09:08:08 -0400

    GroovyEntity

--- src/main/kotlin/engine/entity/entity.kt
+++ src/main/kotlin/engine/entity/entity.kt
@@ -1,81 +1,23 @@
 package engine.entity
 
 import engine.collision.collision
-import engine.collision_box.collisionBox
-import engine.entity.point_entity.PointEntity
+import engine.entity.point_entity.particle.Particle
 import engine.joml_bolt_ons.print
 import engine.tick.tick
 import org.joml.Vector2f
-import org.joml.Vector2fc
 import org.joml.Vector3f
 import org.joml.Vector3fc
 import java.util.*
 
 const val interpolationSnappiness = tick.GOAL
 internal val vector3Worker = Vector3f()
-private val vector2Worker = Vector2f()
+internal val vector2Worker = Vector2f()
 
-private val vel2d = Vector2f()
-private val diff = Vector2f()
-private val accelerationWorker = Vector3f()
-private val goalVel = Vector2f()
+internal val vel2d = Vector2f()
+internal val diff = Vector2f()
+internal val accelerationWorker = Vector3f()
+internal val goalVel = Vector2f()
 
-
-open class GroovyEntity : PointEntity {
-
-  // Thanks, GreenXenith!
-  open val classifier = "undefined"
-  val uuid = UUID.randomUUID().toString()
-  private val size = Vector2f(1f, 1f)
-  private val rotation = Vector3f()
-
-  fun drawCollisionBox() {
-    collisionBox.draw(interpolationPosition, size)
-//    collisionBox.draw(position, size)
-  }
-
-  fun getSize(): Vector2fc = size
-  open fun setSize(newSize: Vector2fc) {
-    size.set(newSize)
-  }
-
-  fun getRotation(): Vector3fc = rotation
-  open fun setRotation(newRotation: Vector3fc) {
-    rotation.set(newRotation)
-  }
-
-
-  fun mobMove(goalX: Float, goalZ: Float, speedGoal: Float, snappiness: Float = 1f) =
-    mobMove(vector2Worker.set(goalX, goalZ), speedGoal, snappiness)
-
-  fun mobMove(goalDir: Vector2fc, speedGoal: Float, snappiness: Float = 1f) {
-
-    goalVel.set(goalDir).normalize().mul(speedGoal)
-
-    val currentVel = getVelocity()
-    val currentAcceleration = getAcceleration()
-
-    vel2d.set(currentVel.x(), currentVel.z())
-
-    goalVel.sub(vel2d, diff)
-    diff.mul(friction * snappiness)
-
-    accelerationWorker.set(
-      diff.x,
-      currentAcceleration.y(),
-      diff.y
-    )
-    if (!accelerationWorker.isFinite) accelerationWorker.set(0f, currentAcceleration.y(), 0f)
-    setAcceleration(accelerationWorker)
-  }
-
-  constructor(pos: Vector3fc) : super(pos)
-
-  open fun onSpawn() {}
-  open fun onDespawn() {}
-  open fun onHit() {}
-  open fun onRightClick() {}
-}
 
 class Item : GroovyEntity {
 
---
+++ src/main/kotlin/engine/entity/point_entity/groovy_entity/GroovyEntity.kt
@@ -0,0 +1,66 @@
+package engine.entity.point_entity.groovy_entity
+
+import engine.collision_box.collisionBox
+import engine.entity.*
+import engine.entity.point_entity.PointEntity
+import org.joml.Vector2f
+import org.joml.Vector2fc
+import org.joml.Vector3f
+import org.joml.Vector3fc
+import java.util.*
+
+open class GroovyEntity : PointEntity {
+
+  // Thanks, GreenXenith!
+  open val classifier = "undefined"
+  val uuid = UUID.randomUUID().toString()
+  private val size = Vector2f(1f, 1f)
+  private val rotation = Vector3f()
+
+  fun drawCollisionBox() {
+    collisionBox.draw(interpolationPosition, size)
+//    collisionBox.draw(position, size)
+  }
+
+  fun getSize(): Vector2fc = size
+  open fun setSize(newSize: Vector2fc) {
+    size.set(newSize)
+  }
+
+  fun getRotation(): Vector3fc = rotation
+  open fun setRotation(newRotation: Vector3fc) {
+    rotation.set(newRotation)
+  }
+
+
+  fun mobMove(goalX: Float, goalZ: Float, speedGoal: Float, snappiness: Float = 1f) =
+    mobMove(vector2Worker.set(goalX, goalZ), speedGoal, snappiness)
+
+  fun mobMove(goalDir: Vector2fc, speedGoal: Float, snappiness: Float = 1f) {
+
+    goalVel.set(goalDir).normalize().mul(speedGoal)
+
+    val currentVel = getVelocity()
+    val currentAcceleration = getAcceleration()
+
+    vel2d.set(currentVel.x(), currentVel.z())
+
+    goalVel.sub(vel2d, diff)
+    diff.mul(friction * snappiness)
+
+    accelerationWorker.set(
+      diff.x,
+      currentAcceleration.y(),
+      diff.y
+    )
+    if (!accelerationWorker.isFinite) accelerationWorker.set(0f, currentAcceleration.y(), 0f)
+    setAcceleration(accelerationWorker)
+  }
+
+  constructor(pos: Vector3fc) : super(pos)
+
+  open fun onSpawn() {}
+  open fun onDespawn() {}
+  open fun onHit() {}
+  open fun onRightClick() {}
+}
